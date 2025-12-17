2a2fe1c41b584067d8118b826efa073816f9d1c8
thakurutkarshsinghrajawat <utkarshsingh21402@gmail.com>
2025-12-17 11:59:56 +0530

    add Button and Footer components; refactor App structure and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import './App.css'
-import {Header} from "./components/organisms/Header.tsx";
+import {Header} from "./components/organisms/Header/Header.tsx";
+import {Footer} from "./components/organisms/footer/Footer.tsx";
+
+
+
 export const App  = () => {
     return (
         <>
         <Header />
+            <Footer />
         </>
     );
 };
+export default App;
